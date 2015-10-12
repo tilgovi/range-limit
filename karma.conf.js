9e5c91eb9f91e0f154e634cb5c61b988b157dc0a
Randall Leeds <randall@bleeds.info>
2015-10-11 19:18:11 -0700

    whitespace

--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,3 @@
-// Karma configuration
-
 var babelify = require('babelify');
 var isparta = require('isparta');
 var istanbul = require('browserify-istanbul')({
@@ -7,17 +5,14 @@ var istanbul = require('browserify-istanbul')({
   instrumenter: isparta
 });
 
-
 module.exports = function(config) {
   config.set({
     browsers: ['PhantomJS'],
     files: ['test/*.js'],
     frameworks: ['browserify', 'chai', 'mocha'],
     preprocessors: {'test/*.js': ['browserify']},
-
     reporters: ['progress', 'coverage', 'coveralls'],
     coverageReporter: {type: 'lcov'},
-
     browserify: {
       debug: true,
       transform: [istanbul, babelify]
